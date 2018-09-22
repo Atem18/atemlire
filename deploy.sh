3ba28d1cd5d46ad9a445b76cb0050644d9bf0201
Kevin Messer <contact@kevin.messer.net>
2018-09-22 22:15:03 +0200

    feat(travis): more stages stuff

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build -t atem18/kmdotnet:$TRAVIS_COMMIT .
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push atem18/kmdotnet:$TRAVIS_COMMIT
+openssl aes-256-cbc -K $encrypted_d7452885b0d3_key -iv $encrypted_d7452885b0d3_iv -in deploy_rsa.enc -out /tmp/deploy_rsa -d
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
+docker run --rm -v /tmp/deploy_rsa:/root/.ssh/id_rsa -v /home/travis/build/Atem18/kmdotnet/_ansible:/ansible/playbooks atem18/ansible-playbook:c9382b16c708777f0f194ae851520a94d6be480d -i www.kevin-messer.net, --extra-vars "TRAVIS_COMMIT=$TRAVIS_COMMIT" kmdotnet.yml
