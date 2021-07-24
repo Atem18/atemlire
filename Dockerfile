5f4b12b331c20e20977b79ca4f41dcd1e9f2a2f6
Atem18 <messer.kevin@gmail.com>
2021-07-24 19:02:04 +0200

    feat(docker): allow npm install in entrypoint

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM jekyll/jekyll:4.2.0
+
+COPY docker-entrypoint.sh /usr/local/bin/
+
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
+
+ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
--- develop.sh
+++ develop.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run --name=atemlire-dev --rm --volume="/Users/atem/Repos/atemlire/app:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:4.2.0 jekyll serve --drafts --incremental
+docker build -t atem18/atemlire:latest .
+
+docker run --name=atemlire-dev --rm --volume="$PWD/app:/srv/jekyll" -p 4000:4000 -it atem18/atemlire:latest jekyll serve --drafts --incremental
